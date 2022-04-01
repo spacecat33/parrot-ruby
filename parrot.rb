f3ed9ff4de2006f58ab6dc948a1b617c51209ebc
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-01 23:11:23 +0100

    Completed assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(greeting = "Squawk!")
+  puts "#{greeting}"
+  "Pretty bird!"
+  "#{greeting}"
+end
+parrot
+parrot("Pretty bird!")
+
+
+
+
